bdcfddb408054b14fb9037fa10e62a59c61605f1
Breno Sonda <breno26sonda@gmail.com>
2022-11-04 19:40:51 -0300

    first commit

---
+++ mobile/src/components/Guesses.tsx
@@ -0,0 +1,96 @@
+import { useState, useEffect } from 'react';
+import { FlatList, useToast } from 'native-base';
+import { api } from '../services/api';
+import { Game, GameProps } from './Game';
+import { EmptyMyPoolList } from './EmptyMyPoolList';
+
+
+interface Props {
+  poolId: string;
+  code: string;
+}
+
+export function Guesses({ poolId, code }: Props) {
+  const toast = useToast();
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [games, setGames] = useState<GameProps[]>([]);
+  const [firstTeamPoints, setFirstTeamPoints] = useState('')
+  const [secondTeamPoints, setSecondTeamPoints] = useState('')
+
+  async function fetchGames() {
+    try {
+      const response = await api.get(`/pools/${poolId}/games`);
+      setGames(response.data.games);
+
+    } catch (err) {
+      console.log(err);
+      toast.show({
+        title: 'Não foi possível carregar jogos',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function handleGuessConfirm(gameId: string) {
+    try {
+      if (!firstTeamPoints.trim() || !secondTeamPoints.trim()) {
+        return toast.show({
+          title: 'Informe o placar do palpite',
+          placement: 'top',
+          bgColor: 'red.500'
+        })
+      }
+
+      await api.post(`/pools/${poolId}/games/${gameId}/guesses`, {
+        firstTeamPoints: Number(firstTeamPoints),
+        secondTeamPoints: Number(secondTeamPoints)
+      });
+
+      toast.show({
+        title: 'Palpite realizado com sucesso!',
+        placement: 'top',
+        bgColor: 'green.500'
+      });
+
+      fetchGames();
+
+
+    } catch (err) {
+      toast.show({
+        title: 'Não foi possível enviar o palpite',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    }
+  }
+
+
+
+  useEffect(() => {
+    fetchGames()
+  }, [poolId])
+
+  return (
+
+    <FlatList
+      data={games}
+      keyExtractor={item => item.id}
+      renderItem={({ item }) => (
+        <Game
+          data={item}
+          setFirstTeamPoints={setFirstTeamPoints}
+          setSecondTeamPoints={setSecondTeamPoints}
+          onGuessConfirm={() => handleGuessConfirm(item.id)}
+
+        />
+      )}
+      _contentContainerStyle={{ pb: 10 }}
+      ListEmptyComponent={() => <EmptyMyPoolList code={code} />}
+    />
+
+  );
+}
---
+++ mobile/src/screens/Pools.tsx
@@ -0,0 +1,71 @@
+import { useCallback, useState } from 'react'
+import { VStack, Icon, useToast, FlatList } from "native-base";
+import { Button } from "../components/Button";
+import { Header } from "../components/Header";
+import { Octicons } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+import { api } from "../services/api";
+import { PoolCard, PoolProps } from '../components/PoolCard';
+import { Loading } from '../components/Loading';
+import { EmptyPoolList } from '../components/EmptyPoolList';
+import { useFocusEffect } from '@react-navigation/native'
+
+export function Pools() {
+    const { navigate } = useNavigation();
+    const toast = useToast();
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [pools, setPools] = useState<PoolProps[]>([]);
+
+    async function fetchPools() {
+        try {
+            const response = await api.get('/pools');
+            setPools(response.data.pools)
+        } catch (err) {
+            console.log(err);
+            toast.show({
+                title: 'Não foi possível carregar os bolões',
+                placement: 'top',
+                bgColor: 'red.500'
+            })
+
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useFocusEffect(useCallback(() => {
+        fetchPools();
+    }, []))
+
+    return (
+        <VStack flex={1} bgColor="gray.900">
+            <Header title="Meus bolões" />
+            <VStack mt={6} mx={5} borderBottomWidth={1} borderBottomColor="gray.600" pb={4} mb={4}>
+                <Button
+                    title="BUSCAR BOLÃO POR CÓDIGO"
+                    leftIcon={<Icon as={Octicons} name="search" color="black" size="md" />}
+                    onPress={() => navigate('find')}
+                />
+            </VStack>
+
+            {isLoading ? <Loading /> :
+
+                <FlatList
+                    data={pools}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => (
+                        <PoolCard
+                            data={item}
+                            onPress={() => navigate('details', { id: item.id })}
+                        />
+                    )}
+                    showsVerticalScrollIndicator={false}
+                    _contentContainerStyle={{ pb: 10 }}
+                    ListEmptyComponent={() => <EmptyPoolList />}
+                    px={5}
+                />
+            }
+        </VStack>
+    )
+}
